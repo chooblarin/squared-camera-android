5f8255b3893e1cbec39ccfc45dfa37a6948d24c5
Souta <choo.bla.rin@gmail.com>
2014-08-31 00:46:13 +0900

    Adjust preview size and SurfaceView

--- app/src/main/java/com/chooblarin/squaredcamera/SquaredCameraPreview.java
+++ app/src/main/java/com/chooblarin/squaredcamera/SquaredCameraPreview.java
@@ -2,10 +2,13 @@ package com.chooblarin.squaredcamera;
 
 import android.content.Context;
 import android.hardware.Camera;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.ViewGroup;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by chooblarin on 2014/08/30.
@@ -38,12 +41,12 @@ public class SquaredCameraPreview extends SurfaceView
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         setWillNotDraw(false); // needed to onDraw call
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.d("mimic", "surfaceChanged: w: " + width + ", h: " + height);
         if (mHolder.getSurface() == null || mCamera == null) {
             return; // preview surface does not exist, or camera instance is unavailable.
         }
@@ -56,6 +59,9 @@ public class SquaredCameraPreview extends SurfaceView
         }
 
         //TODO: set preview size
+        setCameraDisplayOrientation();
+        //setCameraPreviewSizeOld(width, height);
+        setCameraPreviewSize();
 
         mCamera.startPreview();
     }
@@ -67,5 +73,60 @@ public class SquaredCameraPreview extends SurfaceView
             mCamera.release();
             mCamera = null;
         }
+    }
+
+    private void setCameraDisplayOrientation() {
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Camera.getCameraInfo(mCameraId, info);
+        mCamera.setDisplayOrientation(90); // for portrait
+    }
+
+    // set optimal preview size
+    private void setCameraPreviewSize() {
+        Camera.Parameters params = mCamera.getParameters();
+        Log.d(TAG, "current preview size: width ->"
+                + params.getPreviewSize().width
+                + ", height -> " + params.getPreviewSize().height);
+
+        List<Camera.Size> previewSizes = params.getSupportedPreviewSizes();
+        if (previewSizes.isEmpty()) {
+            Log.d(TAG, "emtry preview sizes");
+            return;
+        }
+        Camera.Size bestSize = previewSizes.get(0);
+        for (Camera.Size size : previewSizes) {
+            if (size.width * size.height > bestSize.width * bestSize.height) {
+                bestSize = size;
+            }
+        }
+        params.setPreviewSize(bestSize.width, bestSize.height);
+
+        mCamera.setParameters(params);
+        adjustViewSize(bestSize);
+    }
+
+    // Adjust SurfaceView size
+    private void adjustViewSize(Camera.Size size) {
+        int width = getWidth();
+        int height = getHeight();
+
+        Log.d("mimic adjustViewSize", "get width: " + width);
+        Log.d("mimic adjustViewSize", "get height: " + height);
+
+        Log.d("mimic adjustViewSize", "size width: " + size.width);
+        Log.d("mimic adjustViewSize", "size height: " + size.height);
+
+        ViewGroup.LayoutParams layoutParams = this.getLayoutParams();
+        // for portrait
+        layoutParams.width = width;
+        // float coefficient = (float) width / size.width;
+        float coefficient = (float) size.height / width;
+        Log.d("mimic adjustViewSize", "coefficient: " + coefficient);
+        layoutParams.height = (int)(size.width * coefficient);
+
+        Log.d("mimic layout", "width: " + layoutParams.width
+                + ", height: " + layoutParams.height);
+
+        this.setLayoutParams(layoutParams);
     }
 }
